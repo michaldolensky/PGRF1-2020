e7ab5af8cb1dc8391e4f1c41fbacada803484165
Milan Košťák <milankostak@seznam.cz>
2020-10-05 13:06:49 +0200

    1. cvičení (3)

--- src/pondeli_11_35_c06/app/AppStart.java
+++ src/pondeli_11_35_c06/app/AppStart.java
@@ -1,14 +1,35 @@
 package pondeli_11_35_c06.app;
 
+import pondeli_11_35_c06.rasterize.Raster;
 import pondeli_11_35_c06.view.Window;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AppStart {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Window window = new Window();
+
+            Raster raster = window.getPanel().getRaster();
+
+            raster.setPixel(15, 30, Color.YELLOW.getRGB());
+//            window.getPanel().getRaster().clear();
+            raster.setPixel(10, 10, 0xffff00);
+
+            for (int x = 10; x <= 130; x++) {
+                raster.setPixel(x, 50, 0x00ff00);
+            }
+
+            int x1 = 0, y1 = 1;
+            int x2 = 8, y2 = 4;
+
+            float k = (y2 - y1) / (float) (x2 - x1);
+            float q = y1 - k * x1;
+            System.out.println(k);
+            System.out.println(q);
+
             window.setVisible(true); // zobrazit okno
         });
         // https://www.google.com/search?q=SwingUtilities.invokeLater
--- src/pondeli_11_35_c06/rasterize/RasterBufferedImage.java
+++ src/pondeli_11_35_c06/rasterize/RasterBufferedImage.java
@@ -6,7 +6,7 @@ import java.awt.image.BufferedImage;
 public class RasterBufferedImage implements Raster {
 
     private final BufferedImage img;
-    private int color;
+    private int clearColor;
 
     public RasterBufferedImage(int width, int height) {
         img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
@@ -14,14 +14,14 @@ public class RasterBufferedImage implements Raster {
 
     public void repaint(Graphics graphics) {
         graphics.drawImage(img, 0, 0, null);
+        // pro zájemce - co dělá observer - https://stackoverflow.com/a/1684476
     }
 
     public void draw(RasterBufferedImage raster) {
         Graphics graphics = getGraphics();
-        graphics.setColor(new Color(color));
+        graphics.setColor(new Color(clearColor));
         graphics.fillRect(0, 0, getWidth(), getHeight());
         graphics.drawImage(raster.img, 0, 0, null);
-        // pro zájemce - co dělá observer - https://stackoverflow.com/a/1684476
     }
 
     public BufferedImage getImg() {
@@ -45,13 +45,13 @@ public class RasterBufferedImage implements Raster {
     @Override
     public void clear() {
         Graphics g = img.getGraphics();
-        g.setColor(new Color(color));
+        g.setColor(new Color(clearColor));
         g.clearRect(0, 0, img.getWidth() - 1, img.getHeight() - 1);
     }
 
     @Override
-    public void setClearColor(int color) {
-        this.color = color;
+    public void setClearColor(int clearColor) {
+        this.clearColor = clearColor;
     }
 
     @Override
